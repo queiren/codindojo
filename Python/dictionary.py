db2b07caeef1bc4584d8350cb22cb6aac0801482
Jorge <queiren@gmail.com>
2017-06-11 00:41:17 -0500

    updated file

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -1,3 +1,16 @@
-import requests
-r = requests.get('https://github.com/timeline.json')
-print r.text
+# def info_person():
+#     person = {"name": "Jorge", "age": "31", "country": "Mexico", "programingLang": "Python"}
+#     print "My name is ", person["name"]
+#     print "My age is ", person["age"]
+#     print "My country of birth is ", person["country"]
+#     print "My favorite language is ", person["programingLang"]
+#
+# info_person()
+
+
+def info person():
+	person = {"name": "Jorge", "age": "31", "country": "Mexico", "programingLang": "Python"}
+
+	for key, value in person.items():
+    	print "My ", key, "is ", value
+info_person()
